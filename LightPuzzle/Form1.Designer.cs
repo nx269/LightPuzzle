9d761b731888b5d825ec90d4098be2680b8551f3
Nicholas Cornish <nic.cornish@creditdata.co.za>
2021-03-04 16:06:14 +0000

    Add project files.

---
+++ LightPuzzle/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace LightPuzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // panelLayout
+            // 
+            this.panelLayout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelLayout.ColumnCount = 9;
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.Location = new System.Drawing.Point(0, 27);
+            this.panelLayout.Name = "panelLayout";
+            this.panelLayout.RowCount = 9;
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.panelLayout.Size = new System.Drawing.Size(584, 535);
+            this.panelLayout.TabIndex = 1;
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 566);
+            this.Controls.Add(this.panelLayout);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Light Puzzle";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel panelLayout;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+    }
+}
+
---
+++ LightPuzzle/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightPuzzle
+{
+    public partial class Form1 : Form
+    {
+        List<LightBox> lightBoxes = new List<LightBox>();
+        static int gridSize = 5;
+        public Form1()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void NewGame()
+        {
+            panelLayout.RowCount = gridSize;
+            panelLayout.ColumnCount = gridSize;
+            foreach (RowStyle style in panelLayout.RowStyles)
+            {
+                style.SizeType = SizeType.Percent;
+                style.Height = 100 / gridSize;
+            }
+            foreach (ColumnStyle style in panelLayout.ColumnStyles)
+            {
+                style.SizeType = SizeType.Percent;
+                style.Width = 100 / gridSize;
+            }
+            panelLayout.Controls.Clear();
+            lightBoxes.Clear();
+            Random rd = new Random();
+            int xCoord = 0;
+            int yCoord = 0;
+            int activeCount = 0;
+            for (int i = 0; i < (gridSize * gridSize); i++)
+            {
+                int num = rd.Next(0, 10);
+                bool lightActive = rd.Next(0, 10) > 8;
+                if (lightActive)
+                    activeCount++;
+
+                if (activeCount == 0 && xCoord == (gridSize - 1) && yCoord == (gridSize - 1))
+                    lightActive = true;
+                LightBox lightBox = new LightBox(lightActive, new LightBox.Coordinates(xCoord, yCoord));
+                lightBox.Light.Click += delegate (object sender, EventArgs e) { lightBoxClick(sender, e, lightBox.Location); };
+                lightBoxes.Add(lightBox);
+
+                panelLayout.Controls.Add(lightBox.Light);
+                xCoord++;
+                if (xCoord > (gridSize - 1))
+                {
+                    xCoord = 0;
+                    yCoord++;
+                }
+            }
+
+            foreach (LightBox lightBox in lightBoxes)
+            {
+
+                foreach (LightBox compareLightBox in lightBoxes)
+                {
+                    if (IsNeighbour(lightBox, compareLightBox))
+                        lightBox.Neighbours.Add(compareLightBox);
+
+                }
+            }
+        }
+
+        public bool IsNeighbour(LightBox lightBox, LightBox compareLightBox)
+        {
+            if (lightBox.Location.Column == compareLightBox.Location.Column &&
+                (lightBox.Location.Row == compareLightBox.Location.Row + 1 || lightBox.Location.Row == compareLightBox.Location.Row - 1))
+                return true;
+            else if (lightBox.Location.Row == compareLightBox.Location.Row &&
+                (lightBox.Location.Column == compareLightBox.Location.Column + 1 || lightBox.Location.Column == compareLightBox.Location.Column - 1))
+                return true;
+            else
+                return false;
+        }
+        protected void lightBoxClick(object sender, EventArgs e, LightBox.Coordinates coordinates)
+        {
+            LightBox clickedLightBox = GetLightBoxByCoordinates(coordinates);
+            clickedLightBox.Active = !clickedLightBox.Active;
+            foreach (LightBox neighbours in clickedLightBox.Neighbours)
+            {
+                neighbours.Active = !neighbours.Active;
+            }
+            bool win = CheckWin();
+            if (win)
+            {
+                DialogResult dialogResult = MessageBox.Show("Would you like to try again?", "Well Done", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    NewGame();
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Application.Exit();
+                }
+            }
+            
+
+        }
+
+        private bool CheckWin()
+        {
+            int activeLightBoxes = 0;
+            foreach (LightBox lightbox in lightBoxes)
+            {
+                if (lightbox.Active)
+                    activeLightBoxes++;
+            }
+            if (activeLightBoxes == (gridSize * gridSize))
+                return true;
+            return false;
+        }
+
+        public LightBox GetLightBoxByCoordinates(LightBox.Coordinates coordinates)
+        {
+            LightBox lightBoxMatch = null;
+            foreach (LightBox lightBox in lightBoxes)
+            {
+                if (lightBox.Location == coordinates)
+                    lightBoxMatch = lightBox;
+            }
+            return lightBoxMatch;
+        }
+
+        public class LightBox
+        {
+            private bool _active;
+            public LightBox(bool active, Coordinates location)
+            {
+                Light = new Panel();
+                _active = active;
+                Location = location;
+                Light.BackColor = active ? Color.LimeGreen : Color.DarkGreen;
+                Neighbours = new List<LightBox>();
+            }
+
+            public Panel Light { get; set; }
+            public bool Active
+            {
+                get { return _active; }
+                set
+                {
+                    _active = value;
+                    Light.BackColor = _active ? Color.LimeGreen : Color.DarkGreen;
+                }
+            }
+            public Coordinates Location { get; set; }
+            public List<LightBox> Neighbours { get; set; }
+
+            public class Coordinates
+            {
+                public Coordinates(int row, int column)
+                {
+                    Row = row;
+                    Column = column;
+                }
+
+                public int Row { get; set; }
+                public int Column { get; set; }
+            }
+        }
+
+        private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            gridSize = ShowOptions();
+            NewGame();
+        }
+
+        public static int ShowOptions()
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = "Options",
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = "Choose your grid size" };
+            NumericUpDown txtGridSize = new NumericUpDown() { Left = 50, Top = 50, Width = 400, Minimum = 3, Maximum = 9, Value = 5 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(txtGridSize);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? Convert.ToInt32(txtGridSize.Text) : gridSize;
+        }
+    }
+}
